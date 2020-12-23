06f12cbd79ad9dfbd670604be29b867ae300cfcf
Mubassir-Ali <mubassir11@gmail.com>
2020-12-23 13:00:43 +0500

    get and set weather data

--- app.js
+++ app.js
@@ -1,26 +1,49 @@
 window.addEventListener("load", ()=>{
     let long;
     let lat;
+    let locationTimezone =document.querySelector(".location-timezone");
+    let temperatureDegree =document.querySelector(".temperature-degree");
+    let temperatureDescruption =document.querySelector(".temperature-descruption");
+
 
     if(navigator.geolocation){
         navigator.geolocation.getCurrentPosition(position =>{
+            
             lon =position.coords.longitude;
             lat =position.coords.latitude;
-            const apiKey='20b19a15be9fc0af6796ee8ffb96329f';
-            const api =`http://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units={units}`;
+            const proxy ='https://cors-anywhere.herokuapp.com/';
+
+            //console.log(lat,lon);
+            
+           const api =`${proxy}http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=20b19a15be9fc0af6796ee8ffb96329f`;
 
 
 
             fetch(api)
-            .then(response=>{
-                return response.json;
-            })
+                .then(response=>{
+                    return response.json();
+                })
 
-            .then(data=>{
-                console.log(data);
-            })
+                .then(data=>{
+                    console.log(data);
+                    const temp=data.main.temp;
+                    const desc =data.weather[0].description;
+                    const {name}= data;
+                    
+
+                    //Set DOM Elements from the Weather API
+                    temperatureDegree.textContent=temp;
+                    temperatureDescruption.textContent=`it's ${desc}`;
+                    locationTimezone.textContent=`Time Zone: ${name}`;
+
+                    
+
+                });
+                
         });
 
         
-    }
-})
+     }
+    });
+
+
